a7d3e551b19708914d4dc4b456d5f91cb0f8520a
AshenAloka <alokaashen777@gmail.com>
2025-05-21 08:06:06 +0530

    Create README.ml

---
+++ README.ml
@@ -0,0 +1,54 @@
+# Hi there <img src="https://user-images.githubusercontent.com/1303154/88677602-1635ba80-d120-11ea-84d8-d263ba5fc3c0.gif" width="28px" alt="hi"> 
+
+### 👨🏻‍💻 &nbsp;About Me
+
+![visitors](https://visitor-badge.laobi.icu/badge?page_id=AshenAloka) ![Github](https://img.shields.io/github/followers/AshenAloka?label=Follow&style=social)
+
+💡 &nbsp;Passionate about solving real-world problems with code and learning new technologies.\
+🎓 &nbsp;Currently pursuing a BSc (Hons) in Information Technology with a focus on Software Engineering at the Sri Lanka Institute of Information Technology (SLIIT).\
+🌱 &nbsp;Learning *Java Servlets, **Spring Boot*, and expanding full-stack development skills.\
+🎧 &nbsp;Outside coding, I enjoy listening to music, exploring tech trends, and watching documentaries.\
+✉ &nbsp;Reach me at [alokashen777@gmail.com](mailto:alokashen777@gmail.com)
+
+---
+
+### 🛠 &nbsp;Tech Stack
+
+![Java](https://img.shields.io/badge/-Java-05122A?style=flat&logo=Java&logoColor=FFA518)
+![Python](https://img.shields.io/badge/-Python-05122A?style=flat&logo=python)
+![C](https://img.shields.io/badge/-C-05122A?style=flat&logo=c)
+![C++](https://img.shields.io/badge/-C++-05122A?style=flat&logo=C%2B%2B&logoColor=00599C)
+![HTML](https://img.shields.io/badge/-HTML-05122A?style=flat&logo=HTML5)
+![CSS](https://img.shields.io/badge/-CSS-05122A?style=flat&logo=CSS3&logoColor=1572B6)
+![JavaScript](https://img.shields.io/badge/-JavaScript-05122A?style=flat&logo=javascript)
+![Arduino](https://img.shields.io/badge/-Arduino-05122A?style=flat&logo=arduino)
+![PHP](https://img.shields.io/badge/-PHP-05122A?style=flat&logo=php&logoColor=777BB4)
+![MySQL](https://img.shields.io/badge/-MySQL-05122A?style=flat&logo=mysql&logoColor=00758F)
+![Spring Boot](https://img.shields.io/badge/-Spring%20Boot-05122A?style=flat&logo=springboot)
+![WordPress](https://img.shields.io/badge/-WordPress-05122A?style=flat&logo=wordpress&logoColor=21759B)
+![Photoshop](https://img.shields.io/badge/-Photoshop-05122A?style=flat&logo=adobe-photoshop)
+![Illustrator](https://img.shields.io/badge/-Illustrator-05122A?style=flat&logo=adobe-illustrator)
+![Canva](https://img.shields.io/badge/-Canva-05122A?style=flat&logo=canva)
+![Git](https://img.shields.io/badge/-Git-05122A?style=flat&logo=git)
+![GitHub](https://img.shields.io/badge/-GitHub-05122A?style=flat&logo=github)
+![IntelliJ IDEA](https://img.shields.io/badge/-IntelliJ%20IDEA-05122A?style=flat&logo=intellijidea&logoColor=white)
+![VS Code](https://img.shields.io/badge/-VS%20Code-05122A?style=flat&logo=visualstudiocode&logoColor=007ACC)
+
+---
+
+### ⚙ &nbsp;GitHub Analytics
+
+<p align="center">
+  <a href="https://github.com/AshenAloka">
+    <img height="180em" src="https://github-readme-stats-eight-theta.vercel.app/api?username=AshenAloka&show_icons=true&theme=tokyonight&include_all_commits=true&count_private=true"/>
+    <img height="180em" src="https://github-readme-stats-eight-theta.vercel.app/api/top-langs/?username=AshenAloka&layout=compact&langs_count=8&theme=tokyonight"/>
+  </a>
+</p>
+
+---
+
+### 🤝 Connect with Me
+
+[![LinkedIn](https://img.shields.io/badge/-LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/ashen-aloka-a9bb1b31a/)
+[![Gmail](https://img.shields.io/badge/-alokashen777@gmail.com-D14836?style=for-the-badge&logo=gmail&logoColor=white)](mailto:alokashen777@gmail.com)
+[![Facebook](https://img.shields.io/badge/-Facebook-1877F2?style=for-the-badge&logo=facebook&logoColor=white)](https://web.facebook.com/people/Ashen-Aloka/100067444396565/?_rdc=1&_rdr#)
